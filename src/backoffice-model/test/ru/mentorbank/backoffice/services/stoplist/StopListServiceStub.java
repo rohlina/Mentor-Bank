cf95b7e665ced3f34939c74a406c37dcd38a6990
Anna Rokhlina <anna.rokhlina@gmail.com>
2011-06-17 11:33:29 +0400

    Add getPhysicalStopListInfo in stub

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
@@ -4,7 +4,6 @@ import ru.mentorbank.backoffice.model.stoplist.JuridicalStopListRequest;
 import ru.mentorbank.backoffice.model.stoplist.PhysicalStopListRequest;
 import ru.mentorbank.backoffice.model.stoplist.StopListInfo;
 import ru.mentorbank.backoffice.model.stoplist.StopListStatus;
-import ru.mentorbank.backoffice.services.stoplist.StopListService;
 
 public class StopListServiceStub implements StopListService {
 
@@ -12,25 +11,43 @@ public class StopListServiceStub implements StopListService {
 	public static final String INN_FOR_STOP_STATUS = "22222222222222";
 	public static final String INN_FOR_ASKSECURITY_STATUS = "33333333333333";
 
+	public static final String LASTNAME_FOR_OK_STATUS = "Петров";
+	public static final String LASTNAME_FOR_ASKSECURITY_STATUS = "Иванов";
+	public static final String SERIES_FOR_STOP_STATUS = "0000";
+	public static final String NUMBER_FOR_STOP_STATUS = "000000";
+
 	@Override
 	public StopListInfo getJuridicalStopListInfo(
 			JuridicalStopListRequest request) {
 		StopListInfo stopListInfo = new StopListInfo();
 		stopListInfo.setComment("Комментарий");
-		if (INN_FOR_OK_STATUS.equals(request.getInn())){			
+		if (INN_FOR_OK_STATUS.equals(request.getInn())) {
 			stopListInfo.setStatus(StopListStatus.OK);
 		} else if (INN_FOR_STOP_STATUS.equals(request.getInn())) {
-			stopListInfo.setStatus(StopListStatus.STOP);			
+			stopListInfo.setStatus(StopListStatus.STOP);
 		} else {
-			stopListInfo.setStatus(StopListStatus.ASKSECURITY);			
+			stopListInfo.setStatus(StopListStatus.ASKSECURITY);
 		}
 		return stopListInfo;
 	}
 
 	@Override
 	public StopListInfo getPhysicalStopListInfo(PhysicalStopListRequest request) {
-		//TODO: Реализовать
-		return null;
+		StopListInfo stopListInfo = new StopListInfo();
+		stopListInfo.setComment("Комментарий");
+		if (LASTNAME_FOR_ASKSECURITY_STATUS.equals(request.getLastname())) {
+			if (SERIES_FOR_STOP_STATUS.equals(request.getDocumentSeries())
+					&& NUMBER_FOR_STOP_STATUS.equals(request
+							.getDocumentNumber())) {
+				stopListInfo.setStatus(StopListStatus.STOP);
+			} else {
+				stopListInfo.setStatus(StopListStatus.ASKSECURITY);
+			}
+
+		} else {
+			stopListInfo.setStatus(StopListStatus.OK);
+		}
+		return stopListInfo;
 	}
 
 }
